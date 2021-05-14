a78979ff9683bf3523381886f0dc5ab5554dbba1
Mora-na <41199418+PanDkid@users.noreply.github.com>
2021-05-14 09:52:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 import requests,urllib.request
-
-print('测试成功')
+url = "https://api.chanify.net/v1/sender/CIDJ0YgGEiJBQUVCUjcySlk0UkZFQjUzTzVUTzZJTktHVExIQkRNQ0pVGhQtCe2ZSYlujMUeKq8OPSJeemOxNCIECAEQAQ.K6HCm8GeslfGtG2ipxgGte8OmQxpz29tVHNVfwHXSzI/"
+msg = '推送测试9.47'
+msg = urllib.parse.quote(msg)
+response = urllib.request.urlopen(url+msg)
+print(response.read().decode('utf8'))
