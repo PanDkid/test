654ba32a6ce33feb814bb413c962855a15d32187
Mora-na <41199418+PanDkid@users.noreply.github.com>
2021-05-17 18:22:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,2 @@
 import requests,urllib.request
-url = "https://api.chanify.net/v1/sender/CIDJ0YgGEiJBQUVCUjcySlk0UkZFQjUzTzVUTzZJTktHVExIQkRNQ0pVGhQtCe2ZSYlujMUeKq8OPSJeemOxNCIECAEQAQ.K6HCm8GeslfGtG2ipxgGte8OmQxpz29tVHNVfwHXSzI/"
-msg = '推送测试9.47'
-msg = urllib.parse.quote(msg)
-response = urllib.request.urlopen(url+msg)
 print(response.read().decode('utf8'))
